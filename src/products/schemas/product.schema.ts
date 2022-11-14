cecb30fc753bfd1f7258880dd53451a109e13294
Mateusz Moneta <mateusz.moneta@carscanner.io>
2022-11-14 23:32:38 +0100

    Transform fields

--- src/products/schemas/product.schema.ts
+++ src/products/schemas/product.schema.ts
@@ -1,10 +1,22 @@
 import * as mongoose from 'mongoose';
 
 export const ProductSchema = new mongoose.Schema({
-  id: String,
   name: String,
   price: String,
   updateDate: Number
+}, {
+  toJSON: {
+    transform: (doc, ret) => {
+      const id = ret['_id'];
+      delete ret['_id'];
+      delete ret['__v'];
+
+      return {
+        ...ret,
+        id
+      }
+    }
+  }
 });
 
 ProductSchema.pre('save', function(next) {
